62194cbb178cc5cacf1533f0c3e158fc07836f9e
jb63 <jb63@sanger.ac.uk>
2024-07-15 13:15:44 +0000

    Styled all R files

--- maf2xlsx.R
+++ maf2xlsx.R
@@ -5,26 +5,26 @@ library(dplyr)
 args <- commandArgs(trailingOnly = T)
 
 if (is.na(args[1])) {
-	stop("Input a MAF file")
+  stop("Input a MAF file")
 }
 
 infile <- args[1]
 
-if (! file.exists(infile)) {
-	stop(paste("File", infile, "does not exist"))
+if (!file.exists(infile)) {
+  stop(paste("File", infile, "does not exist"))
 }
 
 maf <- read.table(infile, header = T, sep = "\t", quote = "", stringsAsFactors = F, comment.char = "", check.names = F)
 
-#outfile <- paste0(sub(".maf", "", basename(infile)) ,".xlsx")
-outfile <- paste0(sub(".maf", "", infile) ,".xlsx")
+# outfile <- paste0(sub(".maf", "", basename(infile)) ,".xlsx")
+outfile <- paste0(sub(".maf", "", infile), ".xlsx")
 
-##print(outfile)
+## print(outfile)
 
 # Check for maximum string length
 
 maf2 <- maf %>% mutate(DOMAINS = str_sub(DOMAINS, 1, 1000))
-##print(head(maf2))
+## print(head(maf2))
 print("Limiting DOMAINS column to 1000 characters")
 
 write_xlsx(maf2, path = outfile, col_names = T, format_headers = F)
--- tsv2xlsx.R
+++ tsv2xlsx.R
@@ -1,4 +1,4 @@
-.libPaths(c("/software/team113/dermatlas/R/R-4.2.2/lib/R/library/",.libPaths()))
+.libPaths(c("/software/team113/dermatlas/R/R-4.2.2/lib/R/library/", .libPaths()))
 
 library(writexl)
 library(stringr)
@@ -7,27 +7,24 @@ library(dplyr)
 args <- commandArgs(trailingOnly = T)
 
 if (is.na(args[1])) {
-	stop("Input a TSV file")
+  stop("Input a TSV file")
 }
 
 infile <- args[1]
 
-if (! file.exists(infile)) {
-	stop(paste("File", infile, "does not exist"))
-} 
+if (!file.exists(infile)) {
+  stop(paste("File", infile, "does not exist"))
+}
 
-outfile <- paste0(sub(".tsv|.txt", "", infile) ,".xlsx")
+outfile <- paste0(sub(".tsv|.txt", "", infile), ".xlsx")
 print(paste("Output file is", outfile))
 
 # In cases where different rows have different columns, need to define the number of columns and use fill=T
 
 max <- max(count.fields(infile, sep = "\t"))
-#print(max)
+# print(max)
 
-tsv_file <- read.table(infile, header = F, sep = "\t", quote = "", stringsAsFactors = F, comment.char = "", check.names = F, col.names = paste0("V",seq_len(max)), fill = T)
+tsv_file <- read.table(infile, header = F, sep = "\t", quote = "", stringsAsFactors = F, comment.char = "", check.names = F, col.names = paste0("V", seq_len(max)), fill = T)
 
-#head(tsv_file)
+# head(tsv_file)
 write_xlsx(tsv_file, path = outfile, col_names = F, format_headers = F)
-
-
-
